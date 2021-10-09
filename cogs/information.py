2c6cf22f98f697722174ff13d760615dab190696
Yohann Boniface <edhyjox@gmail.com>
2021-10-09 11:58:42 +0200

    :recycle: Using pincer `add_fields`

--- cogs/information.py
+++ cogs/information.py
@@ -4,10 +4,10 @@ from typing import TYPE_CHECKING, Dict
 
 import inspect
 from pincer.commands import command
-from pincer.objects import Embed
 
 if TYPE_CHECKING:
     from bot import Bot
+    from pincer.objects import Embed
 
 
 class Information:
@@ -19,32 +19,29 @@ class Information:
 
         for path in folders:
             files = (file for file in listdir(path) if file.endswith(".py"))
+
             for file in files:
                 with open(f"{path}/{file}", encoding="utf-8") as f:
                     self.files[file] = f.read()
 
         self.files["Total"] = "\n".join(self.files.values())
 
-    @command(name="code", description="Provide the code info")
+    @command(name="about", description="Provide the code info")
     async def get_code(self) -> Embed:
-        embed = self.client.embed(
+        return self.client.embed(
             title="Code structure",
             description=f"The whole code structure of {self.client.bot}!"
-        )
-
-        for file_name, file in self.files.items():
-            embed.add_field(
-                name=f"> {file_name}",
-                value=inspect.cleandoc(
+        ).add_fields(
+            self.files,
+            map_title=lambda name: f"> {name}",
+            map_values=lambda f: inspect.cleandoc(
                     f"""
-                    - `{len(file):,}` characters
-                    - `{len(file.splitlines()):,}` lines
+                    - `{len(f):,}` characters
+                    - `{len(f.splitlines()):,}` lines
                     """
                 ),
-                inline=True
-            )
-
-        return embed
+            inline=True
+        )
 
 
 setup = Information
