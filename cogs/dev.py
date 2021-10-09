0d152ed8d543f048600daf98c6fa072c8deb80c0
Yohann Boniface <edhyjox@gmail.com>
2021-10-09 02:36:03 +0200

    :recycle: Using embed chaining

--- cogs/dev.py
+++ cogs/dev.py
@@ -18,44 +18,38 @@ class Dev:
         cols: tuple = ("blue", "green", "yellow", "orange", "red")
         mb: int = 1024 ** 2
 
-        _embed = self.bot.embed(title="Bot Stats")
-
         vm = psutil.virtual_memory()
         percent: int = 100 * (vm.used / vm.total)
-        _embed.add_field(
-            name=f":{cols[int(percent // 20)]}_square: __RAM__",
-            value="\n".join(
-                (
-                    f"> `{percent:.3f}` **%**",
-                    f" - `{vm.total / mb:,.3f}` **Mb**",
-                )
-            ),
-        )
-
         cpu_freq, cpu_percent = psutil.cpu_freq(), psutil.cpu_percent()
-        _embed.add_field(
-            name=f":{cols[int(cpu_percent // 20)]}_square: __CPU__",
-            value=(
-                f"> `{cpu_percent:.3f}`**%**\n"
-                f"- `{cpu_freq.current / 1000:.1f}`/"
-                f"`{cpu_freq.max / 1000:.1f}`"
-                " **Ghz**"
-            ),
-        )
-
         disk = psutil.disk_usage(".")
         percent: int = 100 * (disk.used / disk.total)
-        _embed.add_field(
-            name=f":{cols[int(percent // 20)]}_square: __DISK__",
-            value="\n".join(
-                (
-                    f"> `{percent:.3f}` **%**",
-                    f"- `{disk.total / mb:,.3f}` **Mb**",
-                )
-            ),
-        )
 
-        await ctx.send(embed=_embed)
+        await ctx.send(
+            embed=self.bot.embed(title="Bot Stats").add_field(
+                name=f":{cols[int(percent // 20)]}_square: __RAM__",
+                value="\n".join(
+                    (
+                        f"> `{percent:.3f}` **%**",
+                        f" - `{vm.total / mb:,.3f}` **Mb**",
+                    )
+                ),
+            ).add_field(
+                name=f":{cols[int(cpu_percent // 20)]}_square: __CPU__",
+                value=(
+                    f"> `{cpu_percent:.3f}`**%**\n"
+                    f"- `{cpu_freq.current / 1000:.1f}`/"
+                    f"`{cpu_freq.max / 1000:.1f}` **Ghz**"
+                ),
+            ).add_field(
+                name=f":{cols[int(percent // 20)]}_square: __DISK__",
+                value="\n".join(
+                    (
+                        f"> `{percent:.3f}` **%**",
+                        f"- `{disk.total / mb:,.3f}` **Mb**",
+                    )
+                )
+            )
+        )
 
 
 setup = Dev
