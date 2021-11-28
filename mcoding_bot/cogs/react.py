6186523a3e8ece14e28cb350704c2974ea4e3314
Lunarmagpie <Bambolambo0@gmail.com>
2021-11-27 22:23:35 -0500

    :recycle: updated to use message.react()

--- mcoding_bot/cogs/react.py
+++ mcoding_bot/cogs/react.py
@@ -16,21 +16,14 @@ class React:
         self.client = client
         self.rust_search = compile(r"\brust\b", flags=I)
 
-    async def create_reaction(self, message: UserMessage, reaction: str):
-        await message._http.put(
-            f"/channels/{message.channel_id}/messages/{message.id}/reactions/"
-            f"{reaction}/@me",
-            None
-        )
-
     @Client.event
     async def on_message(self, message: UserMessage):
         if message.type == MessageType.GUILD_MEMBER_JOIN:
-            await self.create_reaction(message, "👋")
+            await message.react("👋")
             return
 
         if self.rust_search.findall(message.content) and "🚀" in message.content:
-            await self.create_reaction(message, "🚀")
+            await message.react("🚀")
 
 
 setup = React
