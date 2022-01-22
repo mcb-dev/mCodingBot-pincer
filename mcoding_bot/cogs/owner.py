e051c3f270d28a47ace4c1acb8a5238ed2566954
Circuit <circuitsacul@icloud.com>
2022-01-22 17:56:54 -0500

    Reply so gitpull command doesn't error out on user interface (#21)
    * Reply so gitpull command doesn't error out on user interface
    
    * Add ack to avoid timeout

--- mcoding_bot/cogs/owner.py
+++ mcoding_bot/cogs/owner.py
@@ -22,7 +22,9 @@ class Owner:
     async def gitpull(self, ctx: pincer.objects.MessageContext):
         if ctx.author.id not in self.bot.config.owner_ids:
             return "You don't have permission to do this."
+        await ctx.interaction.ack()
         os.system("git pull")
+        await ctx.reply("Pulled from github, restarting bot...")
         self.bot.close()
 
 
